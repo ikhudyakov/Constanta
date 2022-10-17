99b0594730a7dc5f35bfb51a83cdd0f49c6ccdda
Ilya Khudyakov <ily-khudyakov@yandex.ru>
2022-10-17 13:47:37 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -17,6 +17,9 @@ import (
 	"time"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 func main() {
